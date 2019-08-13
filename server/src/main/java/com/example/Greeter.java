c0f0263c96aa5bf4409dbca7eae0d046aa314afe
Juste G <justeguipi@gmail.com>
2019-08-13 09:30:30 -0400

    fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,9 +12,11 @@ public class Greeter {
 
   }
 
-  /**
-   * This is a method.
-   */
+/**
+ * 
+ * @param someone
+ * @return greeting string
+ */
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
